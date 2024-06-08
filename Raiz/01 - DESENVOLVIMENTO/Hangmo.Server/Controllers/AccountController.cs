057c902c607b0656caf5576f93c6091396a0ffb7
Matias Batista <Matias.Batista@dclsoft.com.br>
2024-06-08 19:03:38 -0300

    Adicionando rota de logout

---
+++ Raiz/01 - DESENVOLVIMENTO/Hangmo.Server/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Hangmo.Repository.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hangmo.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return NoContent();
+        }
+    }
+}
--- Raiz/01 - DESENVOLVIMENTO/Hangmo.Server/Controllers/AppUserController.cs
+++ Raiz/01 - DESENVOLVIMENTO/Hangmo.Server/Controllers/AppUserController.cs
@@ -1,20 +1,15 @@
 ﻿using Hangmo.Repository.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hangmo.Server.Controllers
 {
-    [Route("api/v1/[controller]")]
+    [Route("[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class AppUserController : ControllerBase
     {
-
-       // [HttpGet("{id}")]
-       // public async Task<ActionResult<AppUser>> GetUserById(int id)
-       // {
-
-       // }
 
     }
 }
